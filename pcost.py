9af48bc30e857836adfa2e49ef663293830813f7
Benjamin Swineford <benjamin@mirusresearch.com>
2023-08-14 13:34:38 -0500

    ex 1-4

--- pcost.py
+++ pcost.py
@@ -1,10 +1,21 @@
-total = 0.0
+def portfolio_cost(filename):
+    total_cost = 0.0
 
-for line in open("Data/portfolio.dat"):
-    ticker, shares, price = line.split()
-    shares = int(shares)
-    price = float(price)
-    print(f"ticker:{ticker}, shares:{shares}, price:{price}, cost: {shares * price}")
-    total += shares * price
+    with open(filename) as f:
+        for line in f:
+            fields = line.split()
 
-print(total)
+            try:
+                nshares = int(fields[1])
+                price = float(fields[2])
+                total_cost = total_cost + nshares * price
+
+            # This catches errors in int() and float() conversions above
+            except ValueError as e:
+                print("Couldn't parse:", repr(line))
+                print("Reason:", e)
+
+    return total_cost
+
+
+print(portfolio_cost("../../Data/portfolio3.dat"))
