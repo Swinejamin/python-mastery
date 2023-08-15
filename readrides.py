5803a24d79c8b38ace9e687854ad1e15639b2d0e
Benjamin Swineford <benjamin@mirusresearch.com>
2023-08-15 15:03:24 -0500

    add RideData class

--- readrides.py
+++ readrides.py
@@ -1,6 +1,33 @@
 import csv
 import tracemalloc
+from collections.abc import Sequence
 from collections import namedtuple
+
+
+class RideData(Sequence):
+    def __init__(self):
+        self.routes = []  # Columns
+        self.dates = []
+        self.daytypes = []
+        self.numrides = []
+
+    def __len__(self):
+        # All lists assumed to have the same length
+        return len(self.routes)
+
+    def __getitem__(self, index):
+        return {
+            "route": self.routes[index],
+            "date": self.dates[index],
+            "daytype": self.daytypes[index],
+            "rides": self.numrides[index],
+        }
+
+    def append(self, d):
+        self.routes.append(d["route"])
+        self.dates.append(d["date"])
+        self.daytypes.append(d["daytype"])
+        self.numrides.append((d["rides"]))
 
 
 class Row:
@@ -31,7 +58,7 @@ def read_rides_as_dicts(filename):
     """
     Read the bus ride data as a list of dicts
     """
-    records = []
+    records = RideData()
     with open(filename) as f:
         rows = csv.reader(f)
         headings = next(rows)  # Skip headers
@@ -133,32 +160,83 @@ def read_rides_as_named_tuples(filename):
     return records
 
 
-def read_file_as_type(filename, type_name):
+def read_rides_with_generator(filename):
+    f = open(filename)
+    f_csv = csv.reader(f)
+    headers = next(f_csv)
+
+    # rows = list()
+    #
+    # for (route, date, daytype, rides) in f_csv:
+    #     item = dict(zip(headers, (route, date, daytype, rides)))
+    #     if route == "22" and date == "02/02/2011":
+    #         pprint(item)
+    #
+    #     rows.append(item)
+
+    return (dict(zip(headers, row)) for row in f_csv)
+
+
+def read_rides_as_columns(filename):
+    """
+    Read the bus ride data into 4 lists, representing columns
+    """
+    routes = []
+    dates = []
+    daytypes = []
+    numrides = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            routes.append(row[0])
+            dates.append(row[1])
+            daytypes.append(row[2])
+            numrides.append(int(row[3]))
+    return dict(routes=routes, dates=dates, daytypes=daytypes, numrides=numrides)
+
+
+def read_file_as_type(filename="Data/ctabus.csv", type_name="dict"):
+    tracemalloc.start()
+    tracemalloc.clear_traces()
+
+    results = None
 
     try:
-        if type_name == "tuple":
-            return read_rides_as_tuples(filename)
+        if type_name == "columns":
+            results = read_rides_with_generator(filename)
+
+        if type_name == "generator":
+            results = read_rides_as_columns(filename)
 
         if type_name == "named_tuple":
-            return read_rides_as_tuples(filename)
+            results = read_rides_as_tuples(filename)
 
-        if type_name == "dict":
-            return read_rides_as_dicts(filename)
+        elif type_name == "dict":
+            results = read_rides_as_dicts(filename)
 
-        if type_name == "instance":
-            return read_rides_as_instances(filename)
+        elif type_name == "instance":
+            results = read_rides_as_instances(filename)
 
-        if type_name == "slots":
-            return read_rides_as_instances_with_slots(filename)
+        elif type_name == "slots":
+            results = read_rides_as_instances_with_slots(filename)
+
+        else:
+            results = read_rides_as_tuples(filename)
+
+        current, peak = tracemalloc.get_traced_memory()
+
+        print(
+            f"Memory Use for {type_name}: Current {current}, Peak {format(peak, '08,.1f')}"
+        )
 
     except Exception as e:
         print(e)
 
-    return read_rides_as_tuples(filename)
+    return results
 
 
 if __name__ == "__main__":
-    tracemalloc.start()
 
     # for type_name in ["tuple", "named_tuple", "dict", "instance", "slots"]:
     for function_to_call in [
@@ -168,13 +246,5 @@ if __name__ == "__main__":
         read_rides_as_instances_with_slots,
         read_rides_as_dicts,
     ]:
-        tracemalloc.clear_traces()
-
         # read_file_as_type("Data/ctabus.csv", type_name)
         function_to_call("Data/ctabus.csv")
-
-        current, peak = tracemalloc.get_traced_memory()
-
-        print(
-            f"Memory Use for {function_to_call.__name__}: Current {current}, Peak {format(peak,'08,.1f')}"
-        )
