4d18ed549de868bd729709b4c6524219e21a3c02
Benjamin Swineford <benjamin@mirusresearch.com>
2023-08-16 13:12:32 -0500

    ex 3_4 plus premature optimizing for fun

--- stock.py
+++ stock.py
@@ -7,7 +7,8 @@ import tableformat
 
 
 class Stock:
-    types = (str, int, float)
+    __slots__ = ('name', '_shares', '_price')
+    _types = {"name": str, "shares": int, "price": float}
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -16,27 +17,69 @@ class Stock:
 
     @classmethod
     def from_row(cls, row):
-        try:
-            values = [func(val) for func, val in zip(cls.types, row.values())]
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
 
-            return cls(*values)
+    @classmethod
+    def check_type(cls, value, type_key):
+        type_to_check = cls._types.get(type_key)
+        if not isinstance(value, type_to_check):
+            try:
+                converted_value = type_to_check(value)
 
-        except Exception as e:
-            print(e)
+                print(
+                    f'Value for {type_key} ({value}) converted from {type(value).__name__} to {type_to_check.__name__} ({converted_value})')
 
+                return converted_value
+
+            except ValueError:
+                raise TypeError(
+                    f"Expected {type_to_check.__name__}, received --{value}-- with type {type(value).__name__}")
+
+        return value
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        converted_value = self.check_type(value, 'shares')
+
+        if converted_value < 0:
+            raise ValueError('shares must be >= 0')
+        self._shares = converted_value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        converted_value = self.check_type(value, 'price')
+        if converted_value <= 0:
+            raise ValueError('price must be > 0')
+        self._price = converted_value
+
+    @property
     def cost(self):
-        return self.shares * self.price
+        return self._shares * self._price
 
     def sell(self, nshares):
-        self.shares -= nshares
+        self._shares -= nshares
 
 
 class DStock(Stock):
-    types = (str, int, Decimal)
+    __slots__ = ('name', '_shares', '_price')
+    _types = {"name": str, "shares": int, "price": Decimal}
+
+    @Stock.cost.getter
+    def cost(self):
+        print(type(self._price))
+        return Decimal(self._shares) * self._price
 
 
 def read_portfolio(filename="Data/portfolio.csv", cls=Stock):
-
     rows = reader.read_file_as_type(
         filename=filename,
         type_name="data_collection",
@@ -62,15 +105,14 @@ def print_portfolio(p):
     #     print("%10s %10d %10.2f" % (s.name, s.shares, s.price))
 
 
-portfolio = read_portfolio()
-Dportfolio = read_portfolio(cls=DStock)
+s = Stock("GOOG", 100, 490.1)
 
-print_portfolio(portfolio)
+s.price = 42
+s.price = '42'
+s.price = 42.0
+s.price = Decimal(42.0)
 
-print()
-print()
-print()
-print()
-print()
 
-print_portfolio(Dportfolio)
+# s.price = Decimal(50.000000)
+
+# s.price = "lol"
