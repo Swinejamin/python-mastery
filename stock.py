a356a155f867982a6b80c8558c65dc256ff2d31b
Benjamin Swineford <benjamin@mirusresearch.com>
2023-08-16 12:03:19 -0500

    ex 3_3

--- stock.py
+++ stock.py
@@ -1,15 +1,28 @@
 from pprint import pprint
 from sys import intern
+from decimal import Decimal
 
 import reader
+import tableformat
 
 
 class Stock:
-    def __init__(self, name, shares, price):
+    types = (str, int, float)
 
+    def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        try:
+            values = [func(val) for func, val in zip(cls.types, row.values())]
+
+            return cls(*values)
+
+        except Exception as e:
+            print(e)
 
     def cost(self):
         return self.shares * self.price
@@ -18,7 +31,11 @@ class Stock:
         self.shares -= nshares
 
 
-def read_portfolio(filename="Data/portfolio.csv"):
+class DStock(Stock):
+    types = (str, int, Decimal)
+
+
+def read_portfolio(filename="Data/portfolio.csv", cls=Stock):
 
     rows = reader.read_file_as_type(
         filename=filename,
@@ -29,23 +46,31 @@ def read_portfolio(filename="Data/portfolio.csv"):
     stocks = list()
 
     for row in rows:
+        result = cls.from_row(row)
+        stocks.append(result)
 
-        stocks.append(Stock(row["name"], row["shares"], row["price"]))
-
-    final = [Stock(name, shares, price) for (name, shares, price) in rows]
+    # final = [Stock(name, shares, price) for (name, shares, price) in rows]
 
     return stocks
 
 
 def print_portfolio(p):
+    tableformat.print_table(p, ["name", "shares", "price"])
+    # print("%10s %10s %10s" % ("name", "shares", "price"))
+    # print(("-" * 10 + " ") * 3)
+    # for s in p:
+    #     print("%10s %10d %10.2f" % (s.name, s.shares, s.price))
 
-    print("|  name   |  shares  |   price   |")
-    print("---------- ---------- ---------- ")
-    for line in p:
-        print(f"     {line.name}   |   {line.price}   |   {line.shares}")
 
-
-s = Stock("GOOG", 100, 490.10)
 portfolio = read_portfolio()
+Dportfolio = read_portfolio(cls=DStock)
 
 print_portfolio(portfolio)
+
+print()
+print()
+print()
+print()
+print()
+
+print_portfolio(Dportfolio)
